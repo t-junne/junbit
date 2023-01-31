8405d5d919367a205a6b575076762b54f8e864a6
t-junne <tjunnne@gmail.com>
2023-01-31 23:47:17 +0900

    feat: 헤더 구성

---
+++ front/src/types/index.d.ts
@@ -0,0 +1,14 @@
+declare module '*.otf' {
+  const value: any
+  export = value
+}
+
+declare module '*.png' {
+  const value: any
+  export = value
+}
+
+declare module '*.svg' {
+  const value: any
+  export = value
+}
